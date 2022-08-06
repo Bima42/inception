1afeafd8ba816df889efbc50df6d85c28518c461
Bima42 <tanpau42@gmail.com>
2022-08-06 13:07:55 -0400

    no more connection error on wordpress but issue with the user

--- inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,11 +2,10 @@
 
 if [ ! -d /var/lib/mysql/$DB_NAME ]; then
   service mysql start
-  mysql -u root -e "\
-    CREATE DATABASE IF NOT EXISTS ${DB_NAME}; \
-    CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; \
-    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%'; \
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; \
-    FLUSH PRIVILEGES; \
-    "
+  mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+  mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'"
+  mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%'"
+  mysql -e "FLUSH PRIVILEGES;"
+  mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'";
 fi
+
--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,35 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-until mysql --host=mariadb --user=$DB_USER --password=$DB_PASSWORD -e '\c'; do
-  echo >&2 "mariadb is unavailable - sleeping"
-  sleep 1
-done
+# WP="./wp-cli.phar --path=/wordpress"
+cd /var/www/wordpress
 
-echo >&2 "mariadb is up - start next wordpress bootstrap"
+# $WP core config \
+#    --dbname=$DB_NAME \
+#    --dbuser=$DB_USER \
+#    --dbpass=$DB_PASSWORD \
+#    --dbhost=mariadb \
+#    --locale=en_EN
 
-# if ! wp core is-installed; then
-#  echo >&2 "wordpress is unavailable - start wordpress install"
-#  wp core download --locale=en_EN --version=5.9.1
-  cd/var/www/wordpress
-
-#  wp config create  
-  wp core config \
-    --dbname=$DB_NAME \
-    --dbuser=$DB_USER \
-    --dbpass=$DB_PASSWORD \
-    --dbhost=mariadb \
-    --locale=en_EN
-
-  wp core install \
+wp core install \
     --url=$WP_URL \
     --title="$WP_TITLE" \
     --admin_user="$WP_ADMIN_USER" \
     --admin_email="$WP_ADMIN_MAIL" \
     --admin_password="$WP_ADMIN_PASSWORD"
 
-  wp user create --porcelain \
-    "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
-  cd -
+wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD"
 
-  php-fpm7.3 -F
-# fi
+cd -
+
+php-fpm7.3 -F
